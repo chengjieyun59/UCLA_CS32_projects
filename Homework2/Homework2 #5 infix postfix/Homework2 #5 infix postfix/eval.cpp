b94f722c714a2e45bc94ee319025bbf2c498d89a
chengjieyun59 <chengjieyun59@gmail.com>
2018-02-06 02:05:03 -0800

    DONE!
    Done with all 5 questions!

--- Homework2/Homework2 #5 infix postfix/Homework2 #5 infix postfix/eval.cpp
+++ Homework2/Homework2 #5 infix postfix/Homework2 #5 infix postfix/eval.cpp
@@ -15,31 +15,65 @@
 using namespace std;
 
 // declarations of additional functions
-bool isValid(string infix, const Map& m);
-string conversion(string infix, const Map& m, bool& isValid);
+int howValid(string infix, const Map& m);
+string conversion(string infix, const Map& m);
 
-int evaluate(string infix, const Map& values, string& postfix, int& result){
+int evaluate(string infix, const Map& values, string& postfix, int& result)
+{
+    int status = howValid(infix, values);
     
+    if (status == 0 || status == 2)
+        postfix = conversion(infix, values);
+    if (status == 1 || status == 2)
+        return status;
     
+    stack<char> operandStack; // Initialize the operator stack to empty
+    char ch;
+    ValueType value; // int value
+    char operand1;
+    char operand2;
+    int temp = 0;
     
-    /*
-     pseudocode for the evaluation of the postfix expression:
-     
-     Initialize the operand stack to empty
-     For each character ch in the postfix string
-     if ch is an operand
-     push the value that ch represents onto the operand stack
-     else // ch is a binary operator
-     set operand2 to the top of the operand stack
-     pop the stack
-     set operand1 to the top of the operand stack
-     pop the stack
-     apply the operation that ch represents to operand1 and
-     operand2, and push the result onto the stack
-     When the loop is finished, the operand stack will contain one item,
-     the result of evaluating the expression
-     */
-    return 0;
+    for (int i = 0; i < postfix.size(); i++) // For each character ch in the infix string
+    {
+        ch = postfix[i];
+        
+        // ch is an operand:
+        if (values.get(ch, value))
+            operandStack.push(value); // push the value that ch represents onto the operand stack
+        
+        // ch is a binary operator:
+        else
+        {
+            operand2 = operandStack.top();
+            operandStack.pop();
+            operand1 = operandStack.top();
+            operandStack.pop();
+            
+            switch (ch)
+            {
+                case '*':
+                    temp = operand1 * operand2;
+                    break;
+                case '/':
+                    if (operand2 == 0)
+                        return 3;
+                    temp = operand1 / operand2;
+                    break;
+                case '+':
+                    temp = operand1 + operand2;
+                    break;
+                case '-':
+                    temp = operand1 - operand2;
+                    break;
+                default:
+                    break;
+            } // apply the operation that ch represents to operand1 and operand2
+            operandStack.push(temp); // push the result onto the stack
+        }
+    }
+    result = operandStack.top(); // When the loop is finished, the operand stack will contain one item, the result of evaluating the expression
+    return status;
 }
 // Evaluates an integer arithmetic expression
 //   If infix is a syntactically valid infix integer expression whose
@@ -58,37 +92,67 @@ int evaluate(string infix, const Map& values, string& postfix, int& result){
 //   set to the value of the expression and the function returns 0.
 
 // implementations of additional functions
-bool isValid(string infix, const Map& m){
+int howValid(string infix, const Map& m)
+{
     stack<char> parenBalanced; // to check if parentheses are balanced
+    int operational = 1; // to check if the number of operands is one more than the number of operators
     char ch;
+
+    if (infix.empty()) // 1.a.
+        return 1;
     
     for (int i = 0; i < infix.size(); i++) // For each character ch in the infix string
     {
         ch = infix[i];
         
+        // 1.b.
         if (ch == '(')
             parenBalanced.push(ch); // push to stack if it's an open parenthesis
         else if (ch == ')')
             parenBalanced.pop(); // pop from stack if it's a closed parenthesis
+        
+        // 1.c.
+        else if (ch == '*' || ch == '/' || ch == '+' || ch == '-')
+            operational++;
+        else if (m.contains(ch))
+            operational--;
+                 
         else if (ch != '*' && ch != '/' && ch != '+' && ch != '-' && ch != ' ')
-            if (!m.contains(ch) || !islower(ch))
-                return false;
+        {
+            if(islower(ch) && !m.contains(ch))
+                return 2; // 2.
+            else if (!islower(ch))
+                return 1; // 1.d.
+        }
     }
     if (!parenBalanced.empty()) // if (number of open parenthesis) != (number of closed parenthesis)
-        return false;
-    return true;
+        return 1; // 1.b.
+    
+    if (operational != 0)
+        return 1; // 1.c.
+    
+    return 0;
 }
+// return 1: infix syntax not valid. Need to check:
+    // a. infix cannot be empty
+    // b. the number of open parenthesis needs to be equal to the number of closed parenthesis
+    // c. the number of operands needs to be one more than the number of operators
+    // d. all char needs to be of lowercase
+// return 2: infix syntax valid, but contains at least one lower case letter operant that does not appear in the values map
+// return 3: infix syntax valid, all lower case operand letters appear in the values map, but attempts to divide by zero. Done in evaluate function
+// return 0 otherwise
 
-string conversion(string infix, const Map& m, bool& isValid) {
+string conversion(string infix, const Map& m)
+{
     string postfix = ""; // Initialize postfix to empty
     stack<char> operatorStack; // Initialize the operator stack to empty
-    isValid = true;
     char ch;
     
     for (int i = 0; i < infix.size(); i++) // For each character ch in the infix string
     {
         ch = infix[i];
-        switch (ch) {
+        switch (ch)
+        {
             case ' ':
                 continue;
 
@@ -129,10 +193,7 @@ string conversion(string infix, const Map& m, bool& isValid) {
                 
             // for operands
             default:
-                if (m.contains(ch))
-                {
                     postfix += ch; // append to end of postfix
-                }
                 break;
         }
     }
@@ -153,16 +214,9 @@ int main()
     for (int k = 0; vars[k] != '#'; k++)
         m.insert(vars[k], vals[k]);
     string pf;
-    // int answer;
+    int answer;
+    assert(evaluate("a+ e", m, pf, answer) == 0  && pf == "ae+"  &&  answer == -6);
     
-    assert(isValid("(a+(i-o)", m) == false);
-    assert(isValid("A+(i-o)", m) == false);
-    assert(isValid("(a+e)*o/y", m) == true); // valid expression
-    assert(isValid("(e-o/(y + y)) * i", m) == true); // valid expression
-
-    /*
-    assert(evaluate("a+ e", m, pf, answer) == 0  &&
-           pf == "ae+"  &&  answer == -6);
     answer = 999;
     assert(evaluate("", m, pf, answer) == 1  &&  answer == 999);
     assert(evaluate("a+", m, pf, answer) == 1  &&  answer == 999);
@@ -172,19 +226,14 @@ int main()
     assert(evaluate("y(o+u)", m, pf, answer) == 1  &&  answer == 999);
     assert(evaluate("a+E", m, pf, answer) == 1  &&  answer == 999);
     assert(evaluate("(a+(i-o)", m, pf, answer) == 1  &&  answer == 999);
+    
     // unary operators not allowed:
     assert(evaluate("-a", m, pf, answer) == 1  &&  answer == 999);
-    assert(evaluate("a*b", m, pf, answer) == 2  &&
-           pf == "ab*"  &&  answer == 999);
-    assert(evaluate("y +o *(   a-u)  ", m, pf, answer) == 0  &&
-           pf == "yoau-*+"  &&  answer == -1);
+    assert(evaluate("a*b", m, pf, answer) == 2  && pf == "ab*"  &&  answer == 999);
+    assert(evaluate("y +o *(   a-u)  ", m, pf, answer) == 0  && pf == "yoau-*+"  &&  answer == -1);
     answer = 999;
-    assert(evaluate("o/(y-y)", m, pf, answer) == 3  &&
-           pf == "oyy-/"  &&  answer == 999);
-    assert(evaluate(" a  ", m, pf, answer) == 0  &&
-           pf == "a"  &&  answer == 3);
-    assert(evaluate("((a))", m, pf, answer) == 0  &&
-           pf == "a"  &&  answer == 3);
+    assert(evaluate("o/(y-y)", m, pf, answer) == 3  && pf == "oyy-/"  &&  answer == 999);
+    assert(evaluate(" a  ", m, pf, answer) == 0  && pf == "a"  &&  answer == 3);
+    assert(evaluate("((a))", m, pf, answer) == 0  && pf == "a"  &&  answer == 3);
     cout << "Passed all tests" << endl;
-     */
 }
