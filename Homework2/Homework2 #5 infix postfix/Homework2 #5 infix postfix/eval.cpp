78743a23a6385f5c2bc1ebf8f2095a1666e029d8
chengjieyun59 <chengjieyun59@gmail.com>
2018-02-06 21:25:24 -0800

    Successful parenthesis check
    Check open parenthesis always happen before its matching closed parenthesis

--- Homework2/Homework2 #5 infix postfix/Homework2 #5 infix postfix/eval.cpp
+++ Homework2/Homework2 #5 infix postfix/Homework2 #5 infix postfix/eval.cpp
@@ -112,8 +112,9 @@ string deleteSpace(string& infix)
 int howValid(string infix, const Map& m)
 {
     deleteSpace(infix);
-    stack<char> parenBalanced; // to check if parentheses are balanced
     int operational = 1; // to check if the number of operands is one more than the number of operators
+    int parenthesis = 0; // to check if parentheses is valid
+    //stack<char> parenBalanced; // to check if parentheses are balanced. This method wasn't able to check if a closed parenthesis happen before any matching parenthesis
     char ch;
 
     if (infix.empty())
@@ -134,13 +135,15 @@ int howValid(string infix, const Map& m)
         {
             if (infix[i+1] == '*' || infix[i+1] == '/' || infix[i+1] == '+' || infix[i+1] == '-' || infix[i+1] == ')')
                 return 1; // 1.g.
-            parenBalanced.push(ch); // push to stack if it's an open parenthesis
+            parenthesis++;
+            //parenBalanced.push(ch); // push to stack if it's an open parenthesis
         }
         else if (ch == ')')
         {
             if (islower(infix[i+1]) || infix[i+1] == '(')
                 return 1; // 1.h.
-            parenBalanced.pop(); // pop from stack if it's a closed parenthesis
+            parenthesis--;
+            //parenBalanced.pop(); // pop from stack if it's a closed parenthesis
         }
         
         // 1.c.
@@ -162,10 +165,12 @@ int howValid(string infix, const Map& m)
             if(islower(infix[i+1]) || infix[i+1] == '(')
                 return 1; // 1.i
         }
+        
+        if (parenthesis < 0)
+            return 1; // 1.k.
     }
-    if (!parenBalanced.empty()) // if (number of open parenthesis) != (number of closed parenthesis)
-        return 1; // 1.b.
-    
+    if (parenthesis != 0) // if (number of open parenthesis) != (number of closed parenthesis)
+        return 1;
     if (operational != 0)
         return 1; // 1.c.
     
@@ -182,6 +187,7 @@ int howValid(string infix, const Map& m)
     // h. the element after a closed parenthesis cannot be an operand or an open parenthesis
     // i. the element after an operand cannot be another operand or a open parenthesis
     // j. the element after an operator cannot be another operator or a closed parenthesis
+    // k. there's always a matching open parenthesis before a closed parenthesis, so that )a+e( wouldn't work
 // return 2: infix syntax valid, but contains at least one lower case letter operant that does not appear in the values map
 // return 3: infix syntax valid, all lower case operand letters appear in the values map, but attempts to divide by zero. Done in evaluate function
 // return 0 otherwise
@@ -263,7 +269,8 @@ int main()
     
     assert(evaluate("a+ e", m, pf, answer) == 0  && pf == "ae+"  &&  answer == -6);
     answer = 999;
-    
+    assert(evaluate("e)*(a", m, pf, answer) == 1  &&  answer == 999); // test for parenthesis
+
     // currently testing:
     assert(evaluate(")a", m, pf, answer) == 1  &&  answer == 999); // test for 1e
     assert(evaluate("+ab", m, pf, answer) == 1  &&  answer == 999); // test for 1e
@@ -272,8 +279,7 @@ int main()
     assert(evaluate("((a+i)i)", m, pf, answer) == 1  &&  answer == 999); // test for 1h
     assert(evaluate("ae +/i", m, pf, answer) == 1  &&  answer == 999); // test for 1i
     assert(evaluate("(a++i)/ji", m, pf, answer) == 1  &&  answer == 999); // test for 1j
-    assert(evaluate("a+e)*(a+e", m, pf, answer) == 1  &&  answer == 999); // test for parenthesis
-    
+
     assert(evaluate("", m, pf, answer) == 1  &&  answer == 999);
     assert(evaluate("a+", m, pf, answer) == 1  &&  answer == 999);
     assert(evaluate("a i", m, pf, answer) == 1  &&  answer == 999);
