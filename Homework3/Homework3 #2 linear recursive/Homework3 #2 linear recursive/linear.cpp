68167c80fc38295274e5c15690557bc0361ab2a2
chengjieyun59 <chengjieyun59@gmail.com>
2018-02-13 01:37:36 -0800

    Finished the first 3 recursive functions and tested them
    1. Return false if the somePredicate function returns false for at least one of the array elements; return true otherwise.
    
    2. Return the number of elements in the array for which the somePredicate function returns false.
    
    3. Return the subscript of the first element in the array for which the somePredicate function returns false.  If there is no such element, return -1.

--- Homework3/Homework3 #2 linear recursive/Homework3 #2 linear recursive/linear.cpp
+++ Homework3/Homework3 #2 linear recursive/Homework3 #2 linear recursive/linear.cpp
@@ -26,24 +26,47 @@ bool somePredicate(double x)
 bool allTrue(const double a[], int n)
 {
     if (n == 0)
-        return true; // reached base case and no numbers have failed somePredicate
-    
-    else if (somePredicate(a[n]) == false)
+        return true;
+    if (n<=0)
         return false;
-    
-    return allTrue(a, n-1);
+    if (n == 1)
+        return somePredicate(a[0]); // reached base case and no numbers have failed somePredicate
+    return (allTrue(a+1, n-1) && somePredicate(*a)); // why must this be *a and can't be a[n]?
 }
 
 // 2. Return the number of elements in the array for which the somePredicate function returns false.
 int countFalse(const double a[], int n)
 {
-    return -999;  // This is incorrect.
+    if (n <= 0)
+        return 0;
+    else if (somePredicate(a[0]) == false)
+        return (countFalse(a+1, n-1) + 1); // increment if somePredicate function returns false and keep checking the next element
+    return countFalse(a+1, n-1); // if (somePredicate(a[0]) == true), just keep che-cking the next element
+    
+    // Professor Nachenberg's Lecture 8 slide 52 code also works:
+    /*
+    if (n <= 0)
+        return 0;
+     
+    int total;
+    total = countFalse(a+1, n-1);
+    if (somePredicate(a[0]) == false)
+        total++;
+    return total;
+    */
 }
 
 // 3. Return the subscript of the first element in the array for which the somePredicate function returns false.  If there is no such element, return -1.
 int firstFalse(const double a[], int n)
 {
-    return -999;  // This is incorrect.
+    if (n <= 0)
+        return -1;
+    if (somePredicate(a[0]) == false)
+        return 0;
+    else if (firstFalse(a+1, n-1) == -1)
+        return -1;
+    else // if (somePredicate(a[0]) == true)
+        return (firstFalse(a+1, n-1) + 1); // increment the index
 }
 
 // 4. Return the subscript of the smallest double in the array (i.e., the one whose value is <= the value of all elements).  If more than one element has the same smallest value, return the smallest subscript of such an element.  If the array has no elements to examine, return -1.
@@ -78,13 +101,33 @@ bool includes(const double a1[], int n1, const double a2[], int n2)
 
 int main()
 {
-    double  var1[] = {30, -9, 76, 22, 4, -1};
-    double  var2[] = {30, 9, 76, 22, 4, 1};
+    double var1[] = {1, -2, 3, -4, 5, -6}; // last neg, 3 neg
+    double var2[] = {1, 2, -3, -4, 5, 6}; // 2 neg
+    double var3[] = {-1, -2, 3, -4, 5, 6}; // first neg, 3 neg
+    double var4[] = {30, 9, 76, 22, 4, 1}; // all pos
+    double var5[] = {1}; // all pos, 1 element
+    double var6[] = {}; // empty array
 
     int size = 6;
+    int size5 = 1;
+    int size6 = 0;
     
-    assert(allTrue(var1, size) == false);
-    assert(allTrue(var2, size) == true); // all positive, should all pass somePredicate
+    ////////////////
+    // test cases //
+    ////////////////
     
+    // function 1
+    assert(allTrue(var1, size) == false && allTrue(var2, size) == false && allTrue(var3, size) == false);
+    assert(allTrue(var4, size) == true && allTrue(var5, size5) == true);
+    assert(allTrue(var6, size6) == true);
+
+    // function 2
+    assert(countFalse(var1, size) == 3 && countFalse(var2, size) == 2 && countFalse(var3, size) == 3);
+    assert(countFalse(var4, size) == 0 && countFalse(var5, size5) == 0 && countFalse(var6, size6) == 0);
+    
+    // function 3
+    assert(firstFalse(var1, size) == 1 && firstFalse(var2, size) == 2 && firstFalse(var3, size) == 0);
+    assert(firstFalse(var4, size) == -1 && firstFalse(var5, size5) == -1 && firstFalse(var6, size6) == -1);
+
 }
 
