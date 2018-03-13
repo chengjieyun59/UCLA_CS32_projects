581e63ecbf322636bd6f85d098cf53ba155466ab
chengjieyun59 <chengjieyun59@gmail.com>
2018-03-13 16:45:48 -0700

    Tokenizer implementation pseudocode

--- Project4/Project4/Tokenizer.cpp
+++ Project4/Project4/Tokenizer.cpp
@@ -9,15 +9,39 @@ public:
     TokenizerImpl(string separators);
     vector<string> tokenize(const std::string& s) const;
 private:
+    bool isSeparator(char pos);
+    string m_buffer;
+    string m_token;
+    string m_separator;
 };
 
+// The Tokenizer constructor must initialize a new Tokenizer object. When you construct a Tokenizer object, you pass in a list of separators, e.g., " ,.$-!;".
 TokenizerImpl::TokenizerImpl(string separators)
+:m_buffer(""), m_token(""), m_separator(separators)
 {
+    Tokenizer t(separators);
 }
 
 vector<string> TokenizerImpl::tokenize(const std::string& s) const
 {
-    return vector<string>();  // This compiles, but may not be correct
+    vector<string> tokens;
+    string token;
+    
+    /* pseudocode
+     while haven't exhausted the string yet
+        while (!isSeparator(pos))
+            token += pos;
+            pos++;
+        tokens.push_back(token);
+     */
+    
+    return tokens;  // This compiles, but may not be correct
+}
+
+bool TokenizerImpl::isSeparator(char pos)
+{
+    // todo
+    return false;
 }
 
 //******************** Tokenizer functions ************************************
